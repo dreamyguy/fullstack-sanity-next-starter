e3887824f9c4209766a51cf13be2e7fc42c35ae7
Wallace Sidhrée <i@dreamyguy.com>
2021-05-17 01:03:19 +0200

    :art: Introduce isometric hero image

--- frontend/src/components/modules/HeroSection.js
+++ frontend/src/components/modules/HeroSection.js
@@ -5,12 +5,19 @@ import PropTypes from 'prop-types';
 const HeroSection = ({ pageType, settings }) => (
   <>
     {pageType === 'frontpage' ? (
-      <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+      <div
+        className="bg-gray-900 py-10 sm:py-16 lg:py-8 lg:pb-14 lg:overflow-hidden"
+        style={{
+          backgroundImage: 'url(/images/915_Website_Design_Isometric_Banner--edited.svg)',
+          backgroundSize: 'cover',
+          backgroundBlendMode: 'overlay',
+        }}
+      >
         <div className="mx-auto max-w-7xl lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:gap-8">
             <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-              <div className="lg:py-24">
-                <h1 className="mt-4 tracking-tight font-extrabold text-white sm:mt-5 lg:mt-6">
+              <div className="bg-gray-900 bg-opacity-90 rounded-3xl p-8 my-24">
+                <h1 className="tracking-tight font-extrabold text-white">
                   <span className="block text-4xl sm:text-6xl xl:text-6xl">{settings?.title}</span>
                   <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5 text-2xl sm:text-4xl xl:text-4xl">
                     {settings?.shortDescription}
@@ -19,15 +26,6 @@ const HeroSection = ({ pageType, settings }) => (
                 <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
                   {settings?.description}
                 </p>
-              </div>
-            </div>
-            <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                <img
-                  className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                  src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
-                  alt=""
-                />
               </div>
             </div>
           </div>
